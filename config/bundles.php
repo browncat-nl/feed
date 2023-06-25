3befbeea94a74b0ab73b695c6e817b40b4bb8c8c
LVoogd <lennartvoogd@live.nl>
2023-06-25 11:55:23 +0200

    Introduce monolog dependency
    Will be used for logging

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
