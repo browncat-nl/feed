9c648e6e612f45d4350179ab3f6be881093ce06a
LVoogd <lennartvoogd@live.nl>
2023-06-25 11:56:49 +0200

    Create in-memory implementation of logger
    Will be used in tests

---
+++ src-dev/Common/Infrastructure/Logger/InMemoryLogger.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Dev\Common\Infrastructure\Logger;
+
+use _PHPStan_dcc7b7cff\React\Dns\Model\Record;
+use DateTimeImmutable;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+final class InMemoryLogger implements LoggerInterface
+{
+    /**
+     * @var list<RecordedLog>
+     */
+    public array $recordedLogs = [];
+
+    public function emergency(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::EMERGENCY,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function alert(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::ALERT,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function critical(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::CRITICAL,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function error(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::ERROR,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function warning(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::WARNING,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function notice(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::NOTICE,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function info(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::INFO,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    public function debug(\Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: LogLevel::DEBUG,
+            created: new DateTimeImmutable(),
+        );
+    }
+
+    /**
+     * @param \Stringable|string $level
+     * @param \Stringable|string $message
+     */
+    public function log($level, \Stringable|string $message, array $context = []): void
+    {
+        $this->recordedLogs[] = new RecordedLog(
+            message: $message,
+            context: $context,
+            level: $level,
+            created: new DateTimeImmutable(),
+        );
+    }
+}
