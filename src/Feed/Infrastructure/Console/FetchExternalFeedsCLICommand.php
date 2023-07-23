135e247801efbc5be37d1800227b12282663fe19
LVoogd <lennartvoogd@live.nl>
2023-07-23 12:11:53 +0200

    Add CLI command to fetch and store all feeds

---
+++ src/Feed/Infrastructure/Console/FetchExternalFeedsCLICommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Feed\Infrastructure\Console;
+
+use App\Common\Infrastructure\Messenger\CommandBus\CommandBus;
+use App\Feed\Application\Command\Article\UpdateOrCreateArticleCommand;
+use App\Feed\Application\Service\FeedProvider\FeedProvider;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
+
+#[AsCommand('feed:fetch')]
+class FetchExternalFeedsCLICommand extends Command
+{
+    /**
+     * @param iterable<FeedProvider> $feedProviders
+     */
+    public function __construct(
+        #[TaggedIterator(FeedProvider::class)]
+        private iterable $feedProviders,
+        private CommandBus $commandBus,
+        private LoggerInterface $logger,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $feedItems = [];
+
+        foreach ($this->feedProviders as $feedProvider) {
+            $this->logger->info('[feed:fetch] Fetching feed for source {source}', [
+                'source' => $feedProvider::getSource(),
+            ]);
+
+            $feedItems = array_merge($feedItems, $feedProvider->fetchFeedItems());
+        }
+
+        $this->logger->info('[feed:fetch] Start syncing external feeds with our datastore');
+
+        foreach ($feedItems as $feedItem) {
+            $this->commandBus->handle(new UpdateOrCreateArticleCommand(
+                $feedItem->title,
+                $feedItem->summary,
+                $feedItem->url,
+                $feedItem->updated,
+                $feedItem->source
+            ));
+        }
+
+        $this->logger->info('[feed:fetch] Finished syncing feeds with our datastore');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ tests/Unit/Feed/Infrastructure/Console/FetchExternalFeedsCLICommandTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Unit\Feed\Infrastructure\Console;
+
+use App\Feed\Application\Command\Article\UpdateOrCreateArticleCommand;
+use App\Feed\Application\Service\FeedProvider\FeedItem;
+use App\Feed\Application\Service\FeedProvider\FeedProvider;
+use App\Feed\Infrastructure\Console\FetchExternalFeedsCLICommand;
+use Dev\Common\Infrastructure\Logger\InMemoryLogger;
+use Dev\Common\Infrastructure\Messenger\CommandBus\RecordingCommandBus;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class FetchExternalFeedsCLICommandTest extends TestCase
+{
+    private RecordingCommandBus $commandBus;
+    private LoggerInterface $logger;
+    private FeedProvider $dummyFeedProvider1;
+    private FeedProvider $dummyFeedProvider2;
+
+
+    public function setUp(): void
+    {
+        $this->commandBus = new RecordingCommandBus();
+        $this->logger = new InMemoryLogger();
+
+        $this->dummyFeedProvider1 = new class implements FeedProvider {
+            public static function getSource(): string
+            {
+                return 'dummy_feed_1';
+            }
+
+            public function fetchFeedItems(): array
+            {
+                return [
+                    new FeedItem(
+                        'test title 1.1',
+                        'test summary 1.1',
+                        'https://example.com/test-title-1-1',
+                        new \DateTime('2022-03-03 00:00:00'),
+                        self::getSource(),
+                    )
+                ];
+            }
+        };
+
+        $this->dummyFeedProvider2 = new class implements FeedProvider {
+            public static function getSource(): string
+            {
+                return 'dummy_feed_2';
+            }
+
+            public function fetchFeedItems(): array
+            {
+                return [
+                    new FeedItem(
+                        'test title 2.1',
+                        'test summary 2.1',
+                        'https://example.com/test-title-2-1',
+                        new \DateTime('2022-03-03 00:00:00'),
+                        self::getSource(),
+                    )
+                ];
+            }
+        };
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_retrieve_and_store_the_feed(): void
+    {
+        // Arrange
+        $command = new FetchExternalFeedsCLICommand(
+            [
+                $this->dummyFeedProvider1,
+                $this->dummyFeedProvider2,
+            ],
+            $this->commandBus,
+            $this->logger,
+        );
+
+        $commandTester = new CommandTester($command);
+
+        // Act
+        $commandTester->execute([]);
+
+        // Assert
+        self::assertEquals(new UpdateOrCreateArticleCommand(
+            'test title 1.1',
+            'test summary 1.1',
+            'https://example.com/test-title-1-1',
+            new \DateTime('2022-03-03 00:00:00'),
+            'dummy_feed_1'
+        ), $this->commandBus->shiftCommand());
+
+        self::assertEquals(new UpdateOrCreateArticleCommand(
+            'test title 2.1',
+            'test summary 2.1',
+            'https://example.com/test-title-2-1',
+            new \DateTime('2022-03-03 00:00:00'),
+            'dummy_feed_2'
+        ), $this->commandBus->shiftCommand());
+
+        self::assertTrue($this->commandBus->isEmpty());
+    }
+}
