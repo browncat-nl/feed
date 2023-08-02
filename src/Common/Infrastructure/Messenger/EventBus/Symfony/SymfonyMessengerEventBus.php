8d3f014f4b9f4b4e8868d0869bb8407b77e2fe0e
LVoogd <lennartvoogd@live.nl>
2023-08-02 12:39:06 +0200

    Introduce event bus
    Here we introduce a new bus next to the existing command bus.
    Technically the key difference between the event-
    and commandbus is that the commandbus only has one handler
    (one-to-one) and a event bus can have multiple (one-to-many).
    
    The purpose of the event bus is fastly different then the
    command bus. If a command is dispatched on the command bus we
    expect a handler to do the action the command describes. With
    the event bus we dispatch an event (mostly for telling the
    system we did a thing) and we don't really care who does
    something with it.

---
+++ src/Common/Infrastructure/Messenger/EventBus/Symfony/SymfonyMessengerEventBus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Common\Infrastructure\Messenger\EventBus\Symfony;
+
+use App\Common\Infrastructure\Messenger\EventBus\EventBus;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsAlias(EventBus::class)]
+class SymfonyMessengerEventBus implements EventBus
+{
+    public function __construct(private MessageBusInterface $eventBus)
+    {
+    }
+
+    public function dispatch(object $event): void
+    {
+        try {
+            $this->eventBus->dispatch($event);
+        } catch (HandlerFailedException $handlerFailedException) {
+            $exceptions = $handlerFailedException->getNestedExceptions();
+
+            $firstException = reset($exceptions);
+
+            throw $firstException !== false ? $firstException : $handlerFailedException;
+        }
+    }
+}
