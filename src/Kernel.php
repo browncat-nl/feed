02910102e84a0cbb3dc2b101c5b85f3b4a2426cd
LVoogd <lennartvoogd@live.nl>
2024-01-07 17:09:10 +0100

    👮 Prevent feed providers with duplicate source
    The feed provider source should be unique for each class that extends
    the FeedProvider interface.
    
    In this commit we add a compiler pass who guards against violations of
    this rule. The container won't compile if a developer picks a source
    name that is already taken by another implementation of the
    feed provider interface.

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Common\Infrastructure\Messenger\CommandBus\AsCommandHandler;
 use App\Common\Infrastructure\Messenger\EventBus\AsEventSubscriber;
+use App\Feed\Infrastructure\Framework\CompilerPass\FeedProviderSourceShouldBeUniqueCompilerPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\DependencyInjection\ChildDefinition;
@@ -47,5 +48,8 @@ class Kernel extends BaseKernel
 
             $definition->addTag('messenger.message_handler', $tagAttributes);
         });
+
+        // @todo move to bundle
+        $container->addCompilerPass(new FeedProviderSourceShouldBeUniqueCompilerPass());
     }
 }
